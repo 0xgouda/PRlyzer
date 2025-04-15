1bc0861362834c1464d49c7afca92b4bfbcc902c
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2025-04-15 20:28:11 +0200

    - handle rate limiting - improve error handling

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -7,10 +7,10 @@ from github import Github, GithubException
 from itertools import islice
 import google.generativeai as genai
 
-# small limits to get the best out of AI
-MAX_NUM_OF_FILES = 10
-MAX_SIZE_OF_ORIGINAL_FILE = 4 * 1024 # 4 Kilo byte
-MAX_CHANGED_LINES = 100
+# configurable limits
+MAX_NUM_OF_FILES = 5
+MAX_SIZE_OF_ORIGINAL_FILE = 3 * 1024 # 3 Kilo byte
+MAX_CHANGES_LINES_PER_FILE = 50
 
 # configure Gemini AI
 genai.configure(api_key=getenv("GEMINI_API_KEY"))
@@ -19,11 +19,11 @@ model = genai.GenerativeModel(model_name="gemini-2.0-flash")
 router = APIRouter()
 
 @router.get('/login')
-async def login(request: Request):
+async def github_login(request: Request):
     return await oauth.github.authorize_redirect(request, getenv('CALLBACK_URL'))
 
 @router.get('/auth')
-async def auth(request: Request):
+async def token_auth(request: Request) -> str:
     try:
         token = await oauth.github.authorize_access_token(request)
         return token
@@ -41,8 +41,7 @@ async def pull_request(websocket: WebSocket):
     try:
         PullRequests(**data)
     except:
-        await websocket.send_json(f"Invalid Json Object: {data}")
-        await websocket.close()
+        socketErr(websocket, f"Invalid Json Object: {data}")
         return
         
     github = Github(login_or_token=data["access_token"])
@@ -50,35 +49,40 @@ async def pull_request(websocket: WebSocket):
     try:
         repo = github.get_repo(data["repo_name"], lazy=False)
     except GithubException as e:
-        await websocket.send_json({"error": "Invalid repo name or repo access issue"})
-        await websocket.close()
+        socketErr(websocket, "Invalid repo name or repo access issue")
         return
 
     try:
         pr = repo.get_pull(data["pr_number"])
     except GithubException as e:
-        await websocket.send_json({"error": "Invalid PR number"})
+        socketErr(websocket, "Invalid PR number")
         return
 
     files = list(islice(pr.get_files(), MAX_NUM_OF_FILES))
-    head_sha = pr.head.sha
 
     chat_session = model.start_chat()
     chat_session.send_message(f"My Repo Received A Pull Request with the following body {pr.body}, i am going to send you the files one by one to analyze them for any bugs, errors or incorrect implementations of the ideas provided in Pull Request Body")
 
     for file in files:
         filename = file.filename
-        if file.additions + file.deletions > MAX_CHANGED_LINES:
-            await websocket.send_json(f"Too big file change in {filename}")
+        if file.additions + file.deletions > MAX_CHANGES_LINES_PER_FILE:
+            await websocket.send_json(f"Too big file change in {filename}, limit {MAX_CHANGES_LINES_PER_FILE} lines per file")
             continue
-        diffs = file.patch
 
-        file_meta_data = repo.get_contents(filename, ref=head_sha)
+        file_meta_data = repo.get_contents(filename, ref=pr.head.sha)
         if file_meta_data.size > MAX_SIZE_OF_ORIGINAL_FILE:
-            await websocket.send_json(f"Too big File: {filename}")
+            await websocket.send_json(f"Too big File size: {filename}, limit {MAX_SIZE_OF_ORIGINAL_FILE // 1024} Kb file size")
             continue
         content = file_meta_data.decoded_content
 
-        response = chat_session.send_message(f"analyze the following diffs in the file named {filename} for any errors, bugs or incorrect implementations of the Pull Request Body, original file {content}, diffs {diffs}")
-        await websocket.send_json({"fileName": filename, "response": response.text})
+        try:
+            response = chat_session.send_message(f"analyze the following diffs in the file named {filename} for any errors, bugs or incorrect implementations of the Pull Request Body, provide only the errors in your output nothing more, original file ```{content}```, diffs ```{file.patch}```, ")
+        except Exception as e:
+            socketErr(websocket, f"LLM API Quota/Rate limit")
+            return
+        await websocket.send_json({"fileName": filename, "analysis": response.text})
+    await websocket.close()
+
+async def socketErr(websocket: WebSocket, msg: str):
+    await websocket.send_json({"error": msg})
     await websocket.close()
