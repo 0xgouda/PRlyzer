9df1ee78e99b57ab4e1415f286c24927682b1393
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2025-04-18 16:10:57 +0200

    fix load_dotenv()

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from src.routes import router
 from starlette.middleware.sessions import SessionMiddleware
-from dotenv import load_dotenv
 from os import getenv
 
 version = "v1"
@@ -12,6 +14,5 @@ app = FastAPI(
     version=version,
 )
 
-load_dotenv()
 app.add_middleware(SessionMiddleware, secret_key=getenv("SECRET_KEY"))
 app.include_router(router=router, prefix=f"/api/{version}")
