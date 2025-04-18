79219c13a0a80bf3a986fc6da41ee8b3587386f7
Seif El-Din Sweilam <saifsweelam@gmail.com>
2025-04-18 15:18:10 +0200

    Enhance login component UI and functionality
    - Updated login.component.html to improve layout and user experience with GitHub login.
    - Enhanced styles in login.component.scss for better visual appeal and responsiveness.
    - Modified login.component.ts to include additional Material components and use environment variable for login URL.
    - Added GitHub logo image to assets.
    - Created environment.ts for configuration management.

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatCard, MatCardContent, MatCardTitle, MatCardHeader, MatCardActions, MatCardSubtitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { environment } from '../../../environments/environment';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCard, MatCardTitle, MatCardContent, MatButtonModule],
+  imports: [MatCard, MatCardTitle, MatCardContent, MatCardHeader, MatCardActions, MatButtonModule, MatIcon, MatCardSubtitle],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginUrl = 'http://localhost:8080/login';
-
+  loginUrl = `${environment.baseUrl}/login`;
   
 }
